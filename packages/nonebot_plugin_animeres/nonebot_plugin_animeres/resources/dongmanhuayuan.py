008841b07eea3a563ae01282304b65e6f6420af6
Melodyknit <2711402357@qq.com>
2023-05-23 14:57:46 +0800

    🎨 删除自己的gocq，将部分插件移动到others

--- packages/nonebot_plugin_animeres/nonebot_plugin_animeres/resources/dongmanhuayuan.py
+++
@@ -1,9 +0,0 @@
-from ..typing import BaseSearchEngine, AnimePools
-
-
-class SearchEngine(BaseSearchEngine):
-    name: str = "动漫花园"
-    base_url: str = "https://www.dongmanhuayuan.com"
-
-    async def __call__(self, keyword: str) -> AnimePools:
-        ...
---
+++ run.bat
@@ -0,0 +1,4 @@
+echo "启动bot"
+call conda activate classbot
+
+nb run --reload
