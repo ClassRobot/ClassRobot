e63560328d47c1b098d56ea4cca1032eba704c66
Melodyknit <2711402357@qq.com>
2023-05-26 16:15:52 +0800

    🔥 多余的文件

--- src/plugins/user_manage/manage/user_setting/modify.py
+++ src/plugins/user_manage/manage/user_setting/modify.py
@@ -1,11 +1,3 @@
-'''
-Author: Melodyknit 2711402357@qq.com
-Date: 2023-02-11 20:06:30
-LastEditors: Melodyknit 2711402357@qq.com
-LastEditTime: 2023-05-25 21:00:07
-FilePath: \classbot2\classbot\src\plugins\user_manage\manage\user_setting\modify.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 from typing import Dict, List, Optional
 from utils.manages import User
 from utils.tools import run_sync
