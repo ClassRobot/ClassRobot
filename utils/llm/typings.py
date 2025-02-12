b19ae6f9c0e720fd4dfeb27dc525b655994d4f75
MelodyKnit <2711402357@qq.com>
2025-02-12 18:09:24 +0800

    ✨ 添加OpenAI聊天相关类型定义，增强代码可读性和可维护性

--- utils/llm/typings.py
+++ utils/llm/typings.py
@@ -1,1 +1,10 @@
-
+from openai.types.chat.chat_completion import ChatCompletion as ChatCompletion  # noqa
+from openai.types.chat.chat_completion_message import (  # noqa
+    ChatCompletionMessage as ChatCompletionMessage,
+)
+from openai.types.chat.chat_completion_tool_param import (  # noqa
+    ChatCompletionToolParam as ChatCompletionToolParam,
+)
+from openai.types.chat.chat_completion_message_param import (  # noqa
+    ChatCompletionMessageParam as ChatCompletionMessageParam,
+)
