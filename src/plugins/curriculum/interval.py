e37aced3f8716590a1ce719d6233f9efbe692e71
MelodyKnit <2711402357@qq.com>
2025-02-21 13:41:37 +0800

    🎨 重构消息推送功能，移除旧工具模块并统一使用新的发送模块；优化文件上传逻辑

--- src/plugins/curriculum/interval.py
+++ src/plugins/curriculum/interval.py
@@ -4,7 +4,7 @@ from nonebot import logger
 from utils.models import CurriculumConfig
 from nonebot_plugin_alconna import UniMessage
 from nonebot_plugin_apscheduler import scheduler
-from utils.models.tool import push_user_message, push_group_message
+from utils.send import push_user_message, push_group_message
 
 from .manager import QueryCurriculum
 
--- src/plugins/tasks/__init__.py
+++ src/plugins/tasks/__init__.py
@@ -1,7 +1,13 @@
+from io import BytesIO
+
+from utils import Emoji
+from qrcode import QRCode
 from nonebot import logger
 from utils.tools import StringCard
+from qrcode.image.pil import PilImage
+from utils.send import bot_upload_file
 from utils.models import Tasks, Classes
-from utils import Emoji, bot_upload_file
+from qrcode.image.pure import PyPNGImage
 from nonebot.params import Arg, ArgPlainText
 from nonebot.adapters import Bot, Event, MessageTemplate
 from nonebot_plugin_alconna import UniMessage, AlconnaMatcher
@@ -221,4 +227,17 @@ async def _(
             await matcher.send(UniMessage.file(url=download_url))
     except Exception as e:
         logger.exception(e)
-        await matcher.finish(Emoji.warning("文件发送失败，可以尝试从链接中下载", download_url, sep="\n"))
+        print(download_url)
+        code_img = QRCode()
+        code_img.add_data(download_url)
+        code_img.make(fit=True)
+        image_bytes = BytesIO()
+        print(code_img.make_image())
+        image: PilImage | PyPNGImage = code_img.make_image()
+
+        image.save(image_bytes)
+        await matcher.finish(
+            UniMessage(Emoji.warning("文件发送失败，可以尝试扫码下载"))
+            + UniMessage.image(raw=image_bytes)
+        )
+        # await matcher.finish(Emoji.warning("文件发送失败，可以尝试从链接中下载", download_url, sep="\n"))
