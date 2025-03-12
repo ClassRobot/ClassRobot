270dfb3408a4f2764ea1161d5e144cee870281f8
MelodyKnit <2711402357@qq.com>
2025-03-12 23:37:56 +0800

    ✨ 添加异常处理以确保COS配置安全加载，并记录错误日志

--- utils/tools/cos/config.py
+++ utils/tools/cos/config.py
@@ -1,6 +1,6 @@
-from nonebot import get_driver
 from qcloud_cos import CosConfig
 from pydantic import Extra, BaseModel
+from nonebot import logger, get_driver
 
 
 class Config(BaseModel, extra=Extra.ignore):
@@ -9,15 +9,19 @@ class Config(BaseModel, extra=Extra.ignore):
     region: str | None = None
     bucket: str | None = None
     scheme: str = "https"
-    
+
     def __bool__(self) -> bool:
         return all([self.cos_secret_id, self.cos_secret_key, self.region, self.bucket])
 
 
 plugin_config = Config(**get_driver().config.dict())
-cos_config = CosConfig(
-    Region=plugin_config.region,
-    SecretId=plugin_config.cos_secret_id,
-    SecretKey=plugin_config.cos_secret_key,
-    Scheme=plugin_config.scheme,
-)
+try:
+    cos_config = CosConfig(
+        Region=plugin_config.region,
+        SecretId=plugin_config.cos_secret_id,
+        SecretKey=plugin_config.cos_secret_key,
+        Scheme=plugin_config.scheme,
+    )
+except Exception as e:
+    cos_config = None
+    logger.exception(e)
